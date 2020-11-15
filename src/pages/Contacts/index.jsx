bc34e37c4c9ab86eafc8140b0cfdf4c1829f549f
redsky0043 <57868948+redsky0043@users.noreply.github.com>
2020-11-15 19:49:45 +0200

    add table

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,45 +1,47 @@
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { ContactsTable } from "../ContactsTable";
 import { useContacts } from "./useContacts";
 
-
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        root: {
-            marginTop: theme.spacing(4),
-        },
-        headContainer: {
-            marginBottom: theme.spacing(3),
-        }
-    })
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
 );
 
 export const Contacts = () => {
-    const classes = useStyles();
-    const contacts = useContacts();
+  const classes = useStyles();
+  const contacts = useContacts();
 
-    if (contacts.isLoading) {
-        return <div>...loading</div>
-    }
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h2">
+            Contacts
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <div>...loading</div>;
+            }
 
-    if (contacts.isError) {
-        return <div>...error</div>
-    }
+            if (contacts.isError) {
+              return <div>...error</div>;
+            }
 
-    return (
-        <Container className={classes.root} >
-            <Grid container >
-                <Grid item xs={12} className={classes.headContainer}>
-                    <Typography variant="h3" component="h2" >
-                        Contacts
-                    </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <div> Contacts {contacts.data[0].name.first} </div>
-                </Grid>
-            </Grid>
-        </Container>
-    );
-}
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
